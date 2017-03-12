ee3897b7e61d660887ee00313905c2e1922342c0
Lapalm <anthony_palm@hotmail.co.jp>
2017-03-12 20:38:32 +0000

    Completed Color Tutorial 1

--- openGLSelfTutorial/main.cpp
+++ openGLSelfTutorial/main.cpp
@@ -15,6 +15,9 @@ using namespace std;
 
 //Global Variables
 
+//Light Attribute
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
 // Window dimensions
 const GLuint WIDTH = 800, HEIGHT = 600;
 
@@ -142,67 +145,58 @@ int main() {
 
 	// Setup OpenGL options
 	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	// Build and compile our shader program
-	Shader program("minimal.vert", "minimal.frag");
+	//Shader program("minimal.vert", "minimal.frag");
+	Shader lightingShader("lighting.vert", "lighting.frag");
+	Shader lampShader("lamp.vert", "lamp.frag");
 
 	GLfloat vertices[] = {
-		// Positions        // Texture Coords
+		// Positions     
 
-		- 0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-		0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,
+		0.5f, -0.5f, -0.5f,
+		0.5f,  0.5f, -0.5f,
+		0.5f,  0.5f, -0.5f,
+		-0.5f,  0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
 
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,
+		0.5f, -0.5f,  0.5f,
+		0.5f,  0.5f,  0.5f,
+		0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,
+		-0.5f, -0.5f,  0.5f,
 
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
+		-0.5f, -0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,
 
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,
+		0.5f,  0.5f, -0.5f,
+		0.5f, -0.5f, -0.5f,
+		0.5f, -0.5f, -0.5f,
+		0.5f, -0.5f,  0.5f,
+		0.5f,  0.5f,  0.5f,
 
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,
+		0.5f, -0.5f, -0.5f,
+		0.5f, -0.5f,  0.5f,
+		0.5f, -0.5f,  0.5f,
+		-0.5f, -0.5f,  0.5f,
+		-0.5f, -0.5f, -0.5f,
 
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-	};
-
-	glm::vec3 cubePositions[] = {
-		glm::vec3(0.0f,  0.0f,  0.0f),
-		glm::vec3(2.0f,  5.0f, -15.0f),
-		glm::vec3(-1.5f, -2.2f, -2.5f),
-		glm::vec3(-3.8f, -2.0f, -12.3f),
-		glm::vec3(2.4f, -0.4f, -3.5f),
-		glm::vec3(-1.7f,  3.0f, -7.5f),
-		glm::vec3(1.3f, -2.0f, -2.5f),
-		glm::vec3(1.5f,  2.0f, -2.5f),
-		glm::vec3(1.5f,  0.2f, -1.5f),
-		glm::vec3(-1.3f,  1.0f, -1.5f)
+		-0.5f,  0.5f, -0.5f,
+		0.5f,  0.5f, -0.5f,
+		0.5f,  0.5f,  0.5f,
+		0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f, -0.5f
 	};
 
 	//VBO = Vertex Buffer Object
@@ -212,95 +206,34 @@ int main() {
 								  //specify the order at which we want to draw these vertices in. 
 								  //Example: Use 4 vertices to draw a square using 2 triangles instead of 6. 
 
-	GLuint VBO, VAO, EBO;
-	glGenVertexArrays(1, &VAO);
+	GLuint VBO, containerVAO, EBO;
+	glGenVertexArrays(1, &containerVAO);
 	glGenBuffers(1, &VBO);
 
-	glBindVertexArray(VAO);
+	glBindVertexArray(containerVAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	// Position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
-	// TexCoord attribute
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(2);
 
 	glBindVertexArray(0); // Unbind VAO
 
-	// Load and create a texture 
-	GLuint texture1;
-	GLuint texture2;
-
-	// ====================
-	// Texture 1
-	// ====================
-	/*glGenTextures() takes as input how many textures we want to generate and stores them in a 
-	GLuint array given as its second argument (in our case just a single GLuint)*/
-	glGenTextures(1, &texture1);
-
-	/*we need to bind it so any subsequent texture commands will configure the currently bound texture:*/
-	glBindTexture(GL_TEXTURE_2D, texture1);
-	// All upcoming GL_TEXTURE_2D operations now have effect on this texture object
-	// Set the texture wrapping parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// Set texture wrapping to GL_REPEAT (usually basic wrapping method)
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	// Set texture filtering parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	int width;
-	int	height;
-	// Load image, create texture and generate mipmaps
-	unsigned char* image = SOIL_load_image("container.jpg", &width, &height, 0, SOIL_LOAD_RGB);
-
-	/*
-	glTexImage2D
-	1) The first argument specifies the texture target; setting this to GL_TEXTURE_2D means 
-	this operation will generate a texture on the currently bound texture object at the 
-	same target (so any textures bound to targets GL_TEXTURE_1D or GL_TEXTURE_3D will not be affected).
-
-	2) The second argument specifies the mipmap level for which we want to create a texture 
-	for if you want to set each mipmap level manually, but we'll leave it at the base level which is 0.
-
-	3) The third argument tells OpenGL in what kind of format we want to store the texture. Our image
-	has only RGB values so we'll store the texture with RGB values as well.
-
-	4) The 4th and 5th argument sets the width and height of the resulting texture. 
-	We stored those earlier when loading the image so we'll use the corresponding variables.
-
-	5) The next argument should always be 0 (some legacy stuff).
-
-	6) GLenum format 
-
-	7-8) The 7th and 8th argument specify the format and datatype of the source image. 
-	We loaded the image with RGB values and stored them as chars (bytes) so we'll pass in 
-	the corresponding values.
-
-	9) The last argument is the actual image data.
-	*/
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-	SOIL_free_image_data(image);
-	glBindTexture(GL_TEXTURE_2D, 0); // Unbind texture when done (set second parameter to 0), so we won't accidentily mess up our texture.
-
 	// ===================
-	// Texture 2
+	// CREATE LAMP
 	// ===================
-	glGenTextures(1, &texture2);
-	glBindTexture(GL_TEXTURE_2D, texture2);
-	// Set our texture parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	// Set texture filtering
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	// Load, create texture and generate mipmaps
-	image = SOIL_load_image("awesomeface.png", &width, &height, 0, SOIL_LOAD_RGB);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-	glGenerateMipmap(GL_TEXTURE_2D);
-	SOIL_free_image_data(image);
-	glBindTexture(GL_TEXTURE_2D, 0);
+	//Set the lights VAO only as the vertices are the same for the light object (also a 3D cube)
+	GLuint lightVAO;
+	glGenVertexArrays(1, &lightVAO);
+	glBindVertexArray(lightVAO);
+	//We only need ot bind to the VBO, the container's VBO's data already contains the correct data.
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	//Set the vertex attributes (only position data for our lamp
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	glBindVertexArray(0);
 
 	//Game loop
 	//Checks if GLFW has been instructed to close.
@@ -316,18 +249,9 @@ int main() {
 
 		//Rendering commands go here
 
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f); //Color to clear with
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f); //Color to clear with
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Clears the screen
 		
-		glActiveTexture(GL_TEXTURE0);
-		/*After activating a texture unit, a subsequent
-		glBindTexture call will bind that texture to the currently active texture unit. */
-		glBindTexture(GL_TEXTURE_2D, texture1);
-		/*By setting them via glUniform1i we make sure each uniform sampler corresponds to the proper texture unit.*/
-		glUniform1i(glGetUniformLocation(program.Program, "ourTexture1"), 0); //programs["initShader"]
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, texture2);
-		glUniform1i(glGetUniformLocation(program.Program, "ourTexture2"), 1); //programs["initShader"]
 		//Draw Codes go here
 
 		/*Texture unit GL_TEXTURE0 is always by default activated.
@@ -338,64 +262,63 @@ int main() {
 		which is useful when we'd have to loop over several texture units.
 		*/
 
-		program.Use();
+		lightingShader.Use();
 
-		//Camera View & Transformation
+		GLint objectColorLoc = glGetUniformLocation(lightingShader.Program, "objectColor");
+		GLint lightColorLoc = glGetUniformLocation(lightingShader.Program, "lightColor");
+		glUniform3f(objectColorLoc, 1.0f, 0.5f, 0.31f);
+		glUniform3f(lightColorLoc, 1.0f, 0.5f, 1.0f); // Set light color to white.
 
-		GLfloat radius = 25.0f;
-		GLfloat camX = sin(glfwGetTime()) * radius;
-		GLfloat camZ = cos(glfwGetTime()) * radius;
-		
+		//Create Camera Transformation
 		//View:
-		glm::mat4 view;
-		view = camera.GetViewMatrix();
-
-		//Compute Transformations
-
-		//Model:
-		glm::mat4 model;
-		
-		//By multiplying the vertex coordinates with this model 
-		//matrix we're transforming the vertex coordinates to world coordinates. 
-		model = glm::rotate(model, (GLfloat)glfwGetTime() * glm::radians (-55.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-
-		//Note that we're translating the scene in the reverse direction of where we want to move.
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -10.0f));
+		glm::mat4 view = camera.GetViewMatrix();
 
 		//Projection:
-		glm::mat4 projection;
-
 		//We want to use perspective projection for our scene so we'll declare the projection matrix like this:
 		//glm::perspective (FOV, Aspect Ratio, zNear, zFar)
-		projection = glm::perspective(glm::radians(camera.Zoom), (float)(WIDTH / HEIGHT), 0.1f, 100.0f);
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)(WIDTH / HEIGHT), 0.1f, 100.0f);
 
 		//Get the uniform location 
-		GLint modelLoc = glGetUniformLocation(program.Program, "model");
-		GLint viewLoc = glGetUniformLocation(program.Program, "view");
-		GLint projLoc = glGetUniformLocation(program.Program, "projection");
-		
+		GLint modelLoc = glGetUniformLocation(lightingShader.Program, "model");
+		GLint viewLoc = glGetUniformLocation(lightingShader.Program, "view");
+		GLint projLoc = glGetUniformLocation(lightingShader.Program, "projection");
+
+		//Pass to shader
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
 		//Draw Container
-		glBindVertexArray(VAO);
-		for (GLuint i = 0; i < 10; i++) {
-			glm::mat4 model;
-			model = glm::translate(model, cubePositions[i]);
-			GLfloat angle = glm::radians(20.0f) * i;
+		glBindVertexArray(containerVAO);
+		glm::mat4 model;
 
-			// Every 3rd iteration(including the first) we set the angle using GLFW's time function.
+		//Pass to shader
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
-			if (i % 1 == 0) {
-				angle = (GLfloat)glfwGetTime() * glm::radians(25.0f);
-			}
-			model = glm::rotate(model,  angle * (i + 1), glm::vec3(1.0f, 0.3f, 0.5f));
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glBindVertexArray(0);
 
-			//Pass to shader
-			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-			glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-			glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+		//Draw Lamp 
+		lampShader.Use();
 
-			glDrawArrays(GL_TRIANGLES, 0, 36);
-		}
+		//Get the uniform location 
+		modelLoc = glGetUniformLocation(lampShader.Program, "model");
+		viewLoc = glGetUniformLocation(lampShader.Program, "view");
+		projLoc = glGetUniformLocation(lampShader.Program, "projection");
+
+		//Set Matrices
+		//Pass to shader
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+		model = glm::mat4();
+		model = glm::translate(model, lightPos);
+		model = glm::scale(model, glm::vec3(0.2f)); //Scale down lamp to make a smaller cube
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+		//Draw the light object (using light's vertex attributes)
+		glBindVertexArray(lightVAO);
+
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 		glBindVertexArray(0);
 
 		//Swap the buffers
@@ -406,7 +329,8 @@ int main() {
 	//A VAO stores the glBindBuffer calls when the target is GL_ELEMENT_ARRAY_BUFFER. This means it stores its unbind calls
 	//so make sure you don't unbind the element array buffer before unbinding your VAO, otherwise it doesn't have an EBO
 	//configured. 
-	glDeleteVertexArrays(1, &VAO);
+	glDeleteVertexArrays(1, &containerVAO);
+	glDeleteVertexArrays(1, &lightVAO);
 	glDeleteBuffers(1, &VBO);
 	glDeleteBuffers(1, &EBO);
 
